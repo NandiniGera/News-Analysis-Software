9fefbc1a7944abb6423ff731911af72c31de51f9
NandiniGera <nandini.gera18@gmail.com>
2023-10-27 10:19:47 +0530

    frontend updated

--- frontend/src/components/news.jsx
+++ frontend/src/components/news.jsx
@@ -13,23 +13,40 @@ const news = () => {
 
   return (
     <>
+   <h1 style={{ textAlign: 'center' }}>
+  <span className="bold2">News Analysis Software</span>
+</h1>
+
       {newsData? (
         <>
           <div>
             {newsData?.map((news) => (
-              <div>
-                <p>{news["Categories"]}</p>
-                <p>Title={news["Title"]}</p>
-                <p>description={news["Description"].slice(0, 30) + '...'}</p>
-              </div>
+              // <div>
+              //   <p>{news["Categories"]}</p>
+              //   <p>Title={news["Title"]}</p>
+              //   <p>description={news["Description"].slice(0, 30) + '...'}</p>
+              // </div>
+              <div className="news-item">
+          <p className="category">{news["Categories"]}</p>
+          <p><span className="bold">Title: </span> {news["Title"]}</p>
+          <p><span className="bold">Description: </span> {news["Description"].slice(0, 30) + '...'}</p>
+          <a className="bold4" target="_blank"
+            href={news["URL"]}>
+          Read More
+          </a>
+          <hr className="divider" />
+        </div>
             ))}
           </div>
         </>
       ) : (
         <>
-          <div>
-            Latest Posts are Loading...
-          </div>
+          {/* <div>
+            <h1>Latest Posts are Loading...</h1>
+          </div> */}
+          <h1>
+  <span className="bold3">Latest Posts are loading...</span>
+</h1>
         </>
       )}
     </>
